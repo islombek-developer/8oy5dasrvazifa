d8a2ef8c66a1a13d52081da9c3f6e32e80d76b36
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-07-02 13:13:24 +0300

    sa

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Teacher, Students
 
-# Register your models here.
+admin.site.register(Course)
+
+admin.site.register(Teacher)
+
+admin.site.register(Students)
